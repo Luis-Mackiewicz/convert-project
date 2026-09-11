3c2907d3037751bd66a7608fc9752ce67c0e238e
Luis Miguel Mackiewicz <luismiguelpix@gmail.com>
2026-09-10 23:51:10 -0300

    feat(script):add regex, and money handling

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
+const USD = 5.35;
+const EUR = 6;
+const GBP = 6.08;
+
 const amount = document.getElementById("amount");
-const currency = document.getElementById();
+const currency = document.getElementById("currency");
+const form = document.querySelector("form");
 
 amount.addEventListener("input", () => {
   const hasCharacteresRegex = /\D+/g;
   amount.value = amount.value.replace(hasCharacteresRegex, "");
 });
+
+form.onsubmit = () => {
+  event.preventDefault();
+
+  console.log(currency.value);
+
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US");
+      break;
+
+    case "EUR":
+      convertCurrency(amount.value, EUR, "E");
+      break;
+
+    case "GBP":
+      convertCurrency(amount.value, GBP, "L");
+      break;
+  }
+};
+
+function convertCurrency(amount, price, symbol) {
+  console.log(amount, price, symbol);
+}
